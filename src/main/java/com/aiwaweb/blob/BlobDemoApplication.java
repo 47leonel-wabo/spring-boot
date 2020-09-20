f516f2436f59d2738ad1fce052deb47ac80bbaa2
leonel47 <waboleonel@gmail.com>
2020-09-20 14:34:40 -0500

    Initial commit

---
+++ src/main/java/com/aiwaweb/blob/BlobDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aiwaweb.blob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlobDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlobDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiwaweb/blob/BlobDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiwaweb.blob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlobDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
